5cd7129d2f68a372b8dc83688a0728d44846a3a0
r <rr@localhost.localdomain>
2020-08-19 14:43:12 -0400

    update

--- 0/welcome.cpp
+++ 0/welcome.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int main() {
     string str;
 
-    cout<< "Hello World!!!";
+    cout<< "Hello World!!!"<<endl;
     cout<< "My I know your name: ";
     getline(cin,str);
 
     cout<< "Welcome " <<str;
-    
+
     return 0;
 }
