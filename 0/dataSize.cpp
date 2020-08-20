e6f480addab083125023d904d4796708251307d5
r <rr@localhost.localdomain>
2020-08-20 01:53:51 -0400

    update

---
+++ 0/dataSize.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a;
+    char b;
+    long c;
+    long long d;
+    float e;
+    double f;
+    string g;
+    std::cout << sizeof(a) << std::endl;
+    std::cout << sizeof(b) << std::endl;
+    std::cout << sizeof(c) << std::endl;
+    std::cout << sizeof(d) << std::endl;
+    std::cout << sizeof(e) << std::endl;
+    std::cout << sizeof(f) << std::endl;
+    std::cout << sizeof(g) << std::endl;
+
+    return 0;
+}
